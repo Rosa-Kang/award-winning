9046d28dfe7c781868d83f9e04ede20ad45093a5
Rosa Kang <therosessom@gmail.com>
2025-03-27 17:12:42 -0400

    navBar update with navItems

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,10 @@
-import React, { useRef } from 'react'
+import React, { useRef } from 'react';
+import Button from './Button';
+import { TiLocationArrow } from 'react-icons/ti';
+
+const navItems = [
+    'Nexus', 'Vault', 'Prologue', 'About', 'Contact'
+];
 
 const Navbar = () => {
     const navContainerRef = useRef(null);
@@ -6,9 +12,37 @@ const Navbar = () => {
   return (
     <div
         ref={navContainerRef}
-        className=''
+        className='fixed inset-x-0 top-4 z-50 h-16 border-none transition-all duration-700 sm:inset-x-6'
     >
-        
+        <header
+        className='absolute top-1/2 w-full -translate-y-1/2'
+        >
+            <nav className="flex size-full items-center justify-between p-4">
+                <div className="flex items-center gap-">
+                    <img src="/img/logo.png" alt="logo" className='w-10'/>
+                    <Button 
+                     id="product-button"
+                     title="Products"
+                     rightIcon={<TiLocationArrow />}
+                     containerClass="bg-blue-50 md:flex hidden items-center justify-center gap-1"
+                    />
+                </div>
+
+                <div className="flex h-full items-center">
+                    <div className="hidden md:block">
+                        {navItems.map((item) => (
+                            <a 
+                                key={item}
+                                href={`#${item.toLowerCase}`}
+                                className='nav-hover-btn'
+                            >
+                                {item}
+                            </a>
+                        ))}
+                    </div>
+                </div>
+            </nav>
+        </header>
     </div>
   )
 }
