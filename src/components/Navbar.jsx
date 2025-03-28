0ee14471dc319034df0da430e475e4495e30fa14
Rosa Kang <therosessom@gmail.com>
2025-03-28 11:02:07 -0400

    add audioplay into Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Button from './Button';
 import { TiLocationArrow } from 'react-icons/ti';
 
@@ -7,7 +7,24 @@ const navItems = [
 ];
 
 const Navbar = () => {
+    const [isAudioPlaying, setIsAudioPlaying] = useState(false);
+    const [isIndicatorActive, setIsIndicatorActive] = useState(false);
+   
     const navContainerRef = useRef(null);
+    const audioElementRef = useRef(null);
+
+    const toggleAudioIndicator = ()  => {
+        setIsAudioPlaying((prev) => !prev);
+        setIsIndicatorActive((prev) => !prev);
+    }
+
+    useEffect(()=>{
+        if(isAudioPlaying)  {
+            audioElementRef.current.play();
+        } else {
+            audioElementRef.current.pause();
+        }
+    }, [isAudioPlaying])
 
   return (
     <div
@@ -33,13 +50,34 @@ const Navbar = () => {
                         {navItems.map((item) => (
                             <a 
                                 key={item}
-                                href={`#${item.toLowerCase}`}
+                                href={`#${item.toLowerCase()}`}
                                 className='nav-hover-btn'
                             >
                                 {item}
                             </a>
                         ))}
                     </div>
+
+                    <button 
+                    onClick={toggleAudioIndicator}
+                    className="ml-10 flex items-center space-x-0.5">
+                      
+                        <audio 
+                        ref={audioElementRef}
+                        className='hidden'
+                        src="/audio/loop.mp3"
+                        loop
+                        />
+
+                        {[1,2,3,4].map((bar) => (
+                        <div 
+                        key={bar}
+                        className={`indicator-line ${isIndicatorActive ? 'active':'' }`}
+                        style={{ animationDelay : `${bar * 0.1}s` }}
+                        />
+                        ))}
+
+                    </button>
                 </div>
             </nav>
         </header>
